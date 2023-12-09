c0d4c18dc466d9e0be02213aa9fd47fdcf20e8b8
KiraArensky <kshi.txt@yandex.ru>
2023-12-10 01:04:00 +0500

    5 lab done need php

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-// Получаем кнопку для переключения темы
 const toggleThemeButton = document.querySelector("#toggle-theme");
 
-// Определяем функцию для переключения темы
 function toggleTheme() {
-  // Получаем текущую тему
   const currentTheme = document.body.classList.contains("light-theme") ? "dark-theme" : "light-theme";
 
-  // Добавляем или удаляем класс в зависимости от текущей темы
   document.body.classList.toggle(currentTheme);
 }
 
-// Прикрепляем обработчик события к кнопке
+/////
+
 toggleThemeButton.addEventListener("click", toggleTheme);
+
+const form1 = document.querySelector("#regist_form");
+const form2 = document.querySelector(".done");
+
+form2.style.display = "none";
+
+form1.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  form1.style.display = "none";
+  form2.style.display = "block";
+});
--- php/backend.php
+++ php/backend.php
@@ -1,7 +1,19 @@
 <?php
-        $name = $_POST["custname"];
-        $email = $_POST["custemail"];
-        $number = $_POST["custnumber"];
-        // $shop = $_POST["shop"];
-        echo "Привет, $name!<br><br>Вы оформили предзаказ. В ближайшее время мы напишем вам на $email или позвоним по $number";
-    ?>
+
+if (isset($_POST['submit'])) {
+    // Получить значение поля ввода с именем "name"
+    $name = $_POST['name'];
+
+    // Получить значение поля ввода с именем "email"
+    $email = $_POST['email'];
+
+    $phone = $_POST['phone'];
+    $shop = $_POST['shop'];
+
+    // Выполнить действия с данными формы
+    echo "Имя: $name $shop $phone";
+    echo "<br>";
+    echo "Электронная почта: $email";
+}
+
+?>
