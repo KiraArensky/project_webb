ea9ab6ba6fcff8369fe590cb6996aec47f0f0458
Kira Arensky <108279694+KiraArensky@users.noreply.github.com>
2023-12-11 09:16:37 +0500

    recommit

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scorpio_sauce</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script async type="text/javascript" src="/js/script.js"></script>
+</head>
+<body>
+    <header class="header">
+        <nav class="header__nav_l">
+            <a class="header__text" id="toggle-theme">
+                THEME
+            </a>
+            <a class="header__text" href="https://github.com/KiraArensky/project_webb">
+                GITHUB
+            </a>
+            <a class="header__text" href="#contact_scroll">
+                CONTACT
+            </a>
+        </nav>
+        <a href="#main_scroll"><img src="img/logo_head.svg" alt="header__logo" class="header__logo"></a>
+        <nav class="header__nav_r">
+            <a class="header__text" href="#story_scroll">
+                STORY
+            </a>
+            <a class="header__text" href="#shop_scroll">
+                SHOP
+            </a>
+            <a class="header__text" id="header__rectangle" href="#regist">
+                BUY
+            </a>
+        </nav>
+    </header>
+    <main class="main">
+        <section>
+            <nav class="main__offer" id="main_scroll">
+                <a class="logo">  
+                    <div>
+                        <img src="img/logo_main.svg" alt="main__logo" class="main__logo">
+                        <div class="banner_string">            
+                            <h2 class="main__peper">PUT SOME FIRE IN YOUR BELLY</h2>
+                            <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">
+                        </div>
+                        <img src="img/sause_on_table.jpg" alt="main__table" class="main__table">
+                    </div>              
+                </a>
+                <a class="logo">
+                    <div>
+                        <h1 class="main__title">THE FIRST HOT SAUCE MADE FOR COFFEE</h1>
+                        <img src="img/sauce_with_hand.jpg" alt="main__hand" class="main__logo">
+                    </div>
+                </a>
+            </nav>
+        </section>
+        <div class="banner">
+            <img src="img/scorpio_left.svg" alt="main__scorpio" class="main__scorpio" id="left">
+            <h1 class="main__scorpio">THROW SOME LAVA IN<br>YOUR JAVA</h1>
+            <img src="img/scorpio_right.svg" alt="main__scorpio" class="main__scorpio" id="right">
+        </div>    
+        <section class="menu" id="shop_scroll">
+            <nav>
+                <a class="menu_list">
+                    <div class="product">
+                        <img src="img/fire_circle.svg" alt="main__fire_icon" class="main__fire_icon_l" id="left">
+                        <img src="img/fire_rectangle.svg" alt="main__fire_icon" class="main__fire_icon_l" id="right">
+                        <img src="img/background_house.svg" alt="main__house" class="main__house">
+                        <h3 class="main__text_menu">TANGY AND TINGLY</h3>
+                        <h2 class="main__text_menu">LIGHT ROAST BLEND</h2>
+                        <img src="img/sauce_green_purple.png" alt="main__product" class="main__product">
+                    </div>
+                </a>
+                <a class="menu_list">
+                    <div class="product">
+                        <img src="img/background_house.svg" alt="main__house" class="main__house">
+                        <h3 class="main__text_menu">LIGHT & DARK ROAST</h3>
+                        <h2 class="main__text_menu">UJJO SAMPLER PACK</h2>
+                        <img src="img/two_sauce_green.png" alt="main__product" class="main__product">
+                    </div>
+                </a>
+                <a class="menu_list">
+                    <div class="product">
+                        <img src="img/fire_rectangle.svg" alt="main__fire_icon" class="main__fire_icon_r" id="left">
+                        <img src="img/fire_circle.svg" alt="main__fire_icon" class="main__fire_icon_r" id="right">
+                        <img src="img/background_house.svg" alt="main__house" class="main__house">
+                        <h3 class="main__text_menu">TANGY AND TINGLY</h3>
+                        <h2 class="main__text_menu">DARK ROAST BLEND</h2>
+                        <img src="img/sauce_green_red.png" alt="main__product" class="main__product">
+                    </div>
+                </a>
+            </nav>
+        </section>
+        <marquee direction="right" scrollamount="15"  class="main__peper_all">
+            <div class="main__peper">
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">            
+                <h2 class="main__peper">PUT SOME FIRE IN YOUR BELLY</h2>
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">
+            </div>
+            <div class="main__peper">            
+                <h2 class="main__peper">PUT SOME FIRE IN YOUR BELLY</h2>
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">
+            </div>
+            <div class="main__peper">            
+                <h2 class="main__peper">PUT SOME FIRE IN YOUR BELLY</h2>
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">
+            </div>
+        </marquee>
+        <section class="article">
+            <div class="article">
+            <div class="main__cup">
+                <img src="img/cup_of_sauce.jpg" alt="main__cup">
+            </div>
+            <nav class="article_list">
+                    <div class="article_list_div" id="div_1">
+                        <h1  class="article_list_h1">THE STORY<br>BEHIND THE SAUCE</h1>
+                        <img src="img/women.jpg" alt="main__article__women" class="main__article__women">
+                    </div>
+                    <div class="article_list_div" id="div_2">
+                        <img src="img/background_star.svg" alt="main__back_star" class="main__back_star" id="story_scroll">
+                        <span class="main__article">
+                            HELLO, I’M LAUREN AND AS A LIFELONG<br>ADVENTUROUS EATER, I’VE TRIED A LOT OF<br>CRAZY THINGS, BUT I NEVER THOUGHT HOT<br>SAUCE IN COFFEE WAS A<br>SANE OPTION UNTIL A FRIEND DARED ME TO TRY IT.<br><br>
+
+
+                            AND IT SUCKED.<br><br>
+
+
+                            BUT I WONDERED IF THERE WAS A WAY TO ADD<br>SPICE TO MY COFFEE. WHEN THERE WAS NO<br>EASY SOLUTION, UJJO WAS BORN.<br><br>
+
+
+                            UJJO (OOO-JOE): THE WORD FOR “FIRE” IN<br>KONKANI, SPOKEN ON THE WEST COAST OF<br>INDIA, IS AN HOMAGE AND A LOVE LETTER TO<br>MY DAD, AN INDIAN IMMIGRANT WHO<br>INSTILLED A FIRE IN MY<br>PALATE – AND MY HEART – FROM THE VERY BEGINNING.
+                        </span>
+                    </div>
+            </nav>
+            </div>
+        </section>
+        <marquee direction="right" scrollamount="15"  class="main__peper_all">
+            <div class="main__peper">
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">            
+                <h2 class="main__peper">PUT SOME FIRE IN YOUR BELLY</h2>
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">
+            </div>
+            <div class="main__peper">            
+                <h2 class="main__peper">PUT SOME FIRE IN YOUR BELLY</h2>
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">
+            </div>
+            <div class="main__peper">            
+                <h2 class="main__peper">PUT SOME FIRE IN YOUR BELLY</h2>
+                <img src="img/mini_pepper.svg" alt="main__peper" class="main__peper">
+            </div>
+        </marquee>
+        <div class="container" id="regist">
+            <div class="wrapper">
+              <div class="title"><span>MAKE ORDER</span></div>
+<!-- НАЧАЛО ФОРМЫ -->
+<form method="POST" id="regist_form">
+  <div class="row">
+    <i class="fas fa-user"></i>
+    <input type="text" placeholder="Name" required name="name">
+  </div>
+  <div class="row">
+    <i class="fas fa-user"></i>
+    <input type="email" placeholder="Email" required name="email">
+  </div>
+  <div class="row">
+    <i class="fas fa-user"></i>
+    <input type="tel" placeholder="Number" required name="phone">
+  </div>
+  <div class="radio__main">
+    <div class="radio">
+      <input type="radio" id="shop_1" name="shop" required value="LIGHT ROAST BLEND" />
+      <label for="shop_1">LIGHT ROAST BLEND</label>
+    </div>
+    <div class="radio">
+      <input type="radio" id="shop_2" name="shop" required value="UJJO SAMPLER PACK" />
+      <label for="shop_2">UJJO SAMPLER PACK</label>
+    </div>
+    <div class="radio">
+      <input type="radio" id="shop_3" name="shop" required value="DARK ROAST BLEND" />
+      <label for="shop_3">DARK ROAST BLEND</label>
+    </div>
+  </div>
+  <div class="button">
+    <input type="submit" value="BUY" name="submit">
+  </div>
+</form>
+<!-- КОНЕЦ ФОРМЫ -->
+              <div class="done">
+                <?php
+                {
+                    $name = "name";
+                    $email = $_POST["email"];
+                    $phone = $_POST["phone"];
+                    $shop = $_POST["shop"];
+                }
+                    echo "Hola, $name! Подробности о вашем $shop отправлены на $phone и $email";
+                ?>      
+                </div>
+            </div>
+          </div>
+    </main>
+    <footer>
+        <nav class="footer">
+            <nav class="footer__logo">
+                <div>
+                    <img src="img/big_logo.svg" alt="footer__img" class="footer__img">
+                </div>
+            </nav>
+            <nav class="footer__text" id="contact_scroll">
+                <a class="footer__text_a" id="PRIVACY">
+                    PRIVACY
+                </a>
+                <a class="footer__text_a" id="UJJO">
+                    UJJO LLC, COLUMBUS OH, 43219
+                </a>
+                <a class="footer__text_a" id="INSTAGRAM">
+                    INSTAGRAM
+                </a>
+            </nav>
+        </nav>
+    </footer>
+</body>
+</html>
